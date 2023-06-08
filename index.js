0897827183071791b348fd4f2c8a626ac69a1ab5
Quynh <qpham2594@gmail.com>
2023-06-08 09:35:44 -0400

    initial setup

--- index.js
+++ index.js
@@ -16,89 +16,82 @@ var words = [
 
 // Make variables for elements using getElementById
 
-let currentWord, hiddenWord = " ";
-let corrects = [], incorrects = []; 
-let letterGuess = []
+let correctGuesses = []
+let incorrectGuesses = []
+let guessedLetters = []
 
 var maxGuesses = 10
 var wins = 0
 var loss = 0
-var incorrectLetters = []
 
-var wordGuess = document.getElementById ('word-to-guess')
-var remainingGuess = document.getElementById ('remaining-guesses').textContent = maxGuesses
-var incorrectLetters = document.getElementById ('incorrect-letters')
-var previousWord = document.getElementById ('previous-word')
-var wins = document.getElementById ('wins')
-var loss = document.getElementById ('losses')
+var wordToGuess = document.getElementById('word-to-guess')
+var remainingGuess = document.getElementById('remaining-guesses')
+var incorrectLetters = document.getElementById('incorrect-letters')
+var previousWord = document.getElementById('previous-word')
+var wins = document.getElementById('wins')
+var loss = document.getElementById('losses')
 
+remainingGuess.textContent = maxGuesses
 
+var currentWord = words[Math.floor(Math.random() * words.length)];      // picking random words from the array // random index multiply by the length of the words; math floor to round down
+var tempCurrentWord = currentWord
+var hiddenWord = Array(currentWord.length + 1).join('_');
+
+wordToGuess.innerHTML = hiddenWord
 
 document.onkeyup = function(e)                  // Have program register key presses
 {
-  console.log (e.key)
-  var key = e.key.toLowerCase ()   // filtering keypresses
-  // if (words.indexOf(key) == -1) return ; // if it's not in the array
-  if (words.length === 1 && key.match(/[a-z]/i)) {
-    if (letterGuess.includes(key)) {              // ignoring duplicate guesses
-      return;
+  var key = e.key.toLowerCase()   // filtering keypresses
+
+  if (key.length > 1 || incorrectGuesses.includes(key)) {
+    return;
+  };
+
+  guessedLetters.push(key)
+
+  if (currentWord.includes(key)) {                      // Compare key presses to the random word
+    correctGuesses.push(key);
+    let hiddenWordLetters = hiddenWord.split('')
+    let currentWordLetters = currentWord.split('')
+
+    let indexOfCorrectGuessedLetter = currentWord.indexOf(key)
+
+    currentWordLetters[indexOfCorrectGuessedLetter] = '_'
+    hiddenWordLetters[indexOfCorrectGuessedLetter] = key
+
+    hiddenWord = hiddenWordLetters.join('')
+    currentWord = currentWordLetters.join('')
+
+    wordToGuess.innerHTML = hiddenWord
+
+    if (tempCurrentWord === hiddenWord) {
+      wins.innerText++
+      reset()
+    }
+  } else {
+
+    incorrectGuesses.push(key)
+    incorrectLetters.textContent = incorrectGuesses.join('');
+    remainingGuess.textContent--;
+
+    if (remainingGuess.textContent == 0) {
+      loss.innerText++; 
+      reset()
     }
   }
   
-  var currentWord = words[Math.floor(Math.random() * words.length)];      // picking random words from the array // random index multiply by the length of the words; math floor to round down
-  hiddenWord = randomWord(currentWord);
-  wordGuess.textContent = hiddenWord  
-
-  if (key === currentWord) {                      // Compare key presses to the random word
-    corrects++;
-    wins.textContent = corrects
-    reset()
-  } 
-  else {
-    remainingGuess--
-    incorrectLetters.push(key);
-    remainingGuess.textContent = maxGuesses;
-
-    if (maxGuesses === 0) {
-      loss ++; 
-      reset ()
-
-    }
-  }
-  
-for (let i = 0; i < words.length; i++) {               // Underscore changes to letter if correct // Underscore remains the same if incorrect
-  
-    var currentLetter = wordGuess[i];
-    if (letterGuess.includes(currentLetter)) {
-      wordGuess += currentLetter;
-    } else {
-      wordGuess += "_";
-    }
-} 
-
-function reset () {
-  previousWord.textContent = currentWord;
-  score++;
-  scoreUpdate (); 
-  maxGuesses = 10;
+function reset() {
+  previousWord.innerText = tempCurrentWord;
+  currentWord = words[Math.floor(Math.random() * words.length)];
+  tempCurrentWord = currentWord
+  hiddenWord = Array(currentWord.length + 1).join('_');
+  wordToGuess.innerText = hiddenWord
   remainingGuess.textContent = maxGuesses;
-  incorrects = [];
+  incorrectGuesses = [];
   incorrectLetters.textContent = "";
 }
 
-function scoreUpdate (){
-  currentWord = words[Math.floor(Math.random() * words.length)];
-  wordGuess.textContent () = hiddenWord;
-  remainingGuess.textContent () = maxGuesses;
-
 }
-
-previousWord.textContent () = hiddenWord
-incorrectLetters.textContent () = key
-
-
-}
-
 
 // Incorrect letters will show what key was pressed wrong
 // If same key is pressed, nothing will change
@@ -108,5 +101,3 @@ incorrectLetters.textContent () = key
 // If out of guess or guess incorrectly, losses increases by 1
 // Game resets with new random word (for loop for game reset?)
 // Previous word shows everytime game resets
-
-
